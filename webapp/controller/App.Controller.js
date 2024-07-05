3ff105f1ebe5040c39cf304b453f921b04d735af
GustavoC <gcostapereira22@gmail.com>
2024-07-05 10:48:22 -0300

    Step 9 e Step 10

--- webapp/controller/App.Controller.js
+++ webapp/controller/App.Controller.js
@@ -1,27 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/m/MessageToast"
 ],(Controlador,MessageToast,JSONModel,ResourceModel) => {
-    "usar estrito";
+    "use strict";
     
     return Controlador.extend("ui5.walkthrough.controller.App",{
         onInit() {
             this._count = 0;
-            const oData = {
-               recipienteDeTexto : {
-                  name : ""
-               }
-            };
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-
-            // set i18n model on view
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-            });
-            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello(){
             // read msg from i18n model
--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,14 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"], (XMLView) => {
-    "usar estrito" ;
-    XMLView.create({
-        viewName: "ui5.walkthrough.view.App"
-    }).then((oView) => oView.placeAt("conteudo"));
-});
+sap.ui.define([
+	"sap/ui/core/ComponentContainer"
+], (ComponentContainer) => {
+	"use strict";
 
+	new ComponentContainer({
+		name: "ui5.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("conteudo");
+});
  
