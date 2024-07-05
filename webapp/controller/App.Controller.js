0c9c1dee225ffaae6e0ac5ad667293568301f2e2
GustavoC <gcostapereira22@gmail.com>
2024-07-05 09:35:54 -0300

    contador

--- webapp/controller/App.Controller.js
+++ webapp/controller/App.Controller.js
@@ -4,8 +4,12 @@ sap.ui.define([
     "usar estrito";
     
     return Controlador.extend("ui5.walkthrough.controller.App",{
+        onInit: function () {
+            this._count = 0;
+        },
         onShowHello(){
-        alert("OLOKO BICHO");
-        }
+            this._count++;                  
+            alert("OLOKO BICHO\nContador: " + this._count);
+        },   
    });
 });
