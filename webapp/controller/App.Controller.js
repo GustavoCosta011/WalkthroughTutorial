54acf3fecbe203a653cb56ddaf679691d940882e
GustavoC <gcostapereira22@gmail.com>
2024-07-05 09:56:32 -0300

    Step 6 e Step 7

--- webapp/controller/App.Controller.js
+++ webapp/controller/App.Controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller" 
-],(Controlador) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+],(Controlador,MessageToast,JSONModel) => {
     "usar estrito";
     
     return Controlador.extend("ui5.walkthrough.controller.App",{
-        onInit: function () {
+        onInit() {
             this._count = 0;
+            const oData = {
+               recipienteDeTexto : {
+                  name : ""
+               }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
         },
         onShowHello(){
             this._count++;                  
-            alert("OLOKO BICHO\nContador: " + this._count);
+            MessageToast.show("OLOKO BICHO\nContador: " + this._count);
         },   
    });
 });
