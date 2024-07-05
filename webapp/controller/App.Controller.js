c4009ef94db3b05e0c92d7571df6d076a35209a0
GustavoC <gcostapereira22@gmail.com>
2024-07-05 13:17:02 -0300

    Step 15

--- webapp/controller/App.Controller.js
+++ webapp/controller/App.Controller.js
@@ -5,17 +5,5 @@ sap.ui.define([
     "use strict";
     
     return Controlador.extend("ui5.walkthrough.controller.App",{
-        onInit() {
-            this._count = 0;
-        },
-        onShowHello(){
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipienteDeTexto/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            this._count++;                  
-            MessageToast.show(sMsg +"\n" + "Contador: " + this._count);
-        },   
    });
 });
