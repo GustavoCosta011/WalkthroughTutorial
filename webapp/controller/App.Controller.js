0a57c5d7ce2a986136e50dc53955c92fd6dc57d8
GustavoC <gcostapereira22@gmail.com>
2024-07-05 10:16:20 -0300

    Step 8

--- webapp/controller/App.Controller.js
+++ webapp/controller/App.Controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-],(Controlador,MessageToast,JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+],(Controlador,MessageToast,JSONModel,ResourceModel) => {
     "usar estrito";
     
     return Controlador.extend("ui5.walkthrough.controller.App",{
@@ -15,10 +16,21 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            // set i18n model on view
+            const i18nModel = new ResourceModel({
+                bundleName: "ui5.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello(){
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipienteDeTexto/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
             this._count++;                  
-            MessageToast.show("OLOKO BICHO\nContador: " + this._count);
+            MessageToast.show(sMsg +"\n" + "Contador: " + this._count);
         },   
    });
 });
