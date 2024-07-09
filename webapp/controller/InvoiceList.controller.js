4fe7f4caadef9b6567174d2b867bd3de624713c4
GustavoC <gcostapereira22@gmail.com>
2024-07-09 12:23:39 -0300

    Step 22

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-		"../model/formatter"
+	"../model/formatter"
 ], (Controller, JSONModel, formatter) => {
 	"use strict";
 
