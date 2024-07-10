b00ac4df9a779dd8ac9b34e48caa8da12c97cceb
GustavoC <gcostapereira22@gmail.com>
2024-07-10 10:44:28 -0300

    Step 32

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], (Controller) => {
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], (Controller, History) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.Detail", {
@@ -13,7 +14,19 @@ sap.ui.define([
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
-			});
+			});  
+		},
+
+		onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
